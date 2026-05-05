be946203fc1e20fd181444d4e152bf0a9d264dda
Davide Boschetti <bdavide1999@gmail.com>
2026-05-05 22:35:54 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === cambia SOLO questa riga per forzare un refresh completo ===
-const CACHE = 'gpsdivider-05-05-2026-1';
+const CACHE = 'gpsdivider-05-05-2026-2';
 // ==============================================================================
 
 const ASSETS = [
